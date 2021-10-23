8de83e15bd03a57669330d48496fe186d5b7845a
Marijn Huizendveld <marijn@round84.com>
2021-10-23 13:00:29 +0200

    :mag: Identify test scenarios.

--- Money/Money.spec.ts
+++ Money/Money.spec.ts
@@ -1,4 +1,4 @@
-import {Expect, Test} from 'alsatian';
+import {Expect, Ignore, Test} from 'alsatian';
 import {Euro} from "./Money";
 
 export class MoneySpec {
@@ -8,5 +8,20 @@ export class MoneySpec {
     @Test()
     Money_Exposes_Currency_IsoCode_for_Euro() {
         Expect(Euro).toBe("EUR");
+    }
+
+    @Test()
+    @Ignore('TODO: Implement this test scenario')
+    Money_equalTo_detects_identical_values() {
+    }
+
+    @Test()
+    @Ignore('TODO: Implement this test scenario')
+    Money_equalTo_detects_difference_of_currencies() {
+    }
+
+    @Test()
+    @Ignore('TODO: Implement this test scenario')
+    Money_equalTo_detects_difference_in_amount() {
     }
 }
