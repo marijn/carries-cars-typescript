b1cb72df0ad57bdbe132261eb5ce2f6fafbf4648
Marijn Huizendveld <marijn@round84.com>
2021-10-23 15:26:05 +0200

    :white_check_mark: `CalculatePrice_charged_per_minute`

--- Money/Money.ts
+++ Money/Money.ts
@@ -1,9 +1,22 @@
 export const Euro = "";
 
+export type Currencies = "EUR" | "USD";
+
+class money implements Money{
+    constructor(
+        readonly amountInCents: number,
+        readonly currency: Currencies
+    ) {}
+
+    equalTo(other: money): boolean {
+        return other.amountInCents === this.amountInCents && other.currency === this.currency;
+    }
+}
+
 export type Money = {
     equalTo(other: Money): boolean;
 }
 
 export function EUR(amountInCents: number): Money {
-    return;
+    return new money(amountInCents, "EUR");
 }
