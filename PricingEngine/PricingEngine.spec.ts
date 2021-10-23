b8d8bb46bca0947402a3e109677c04a537f630a1
Marijn Huizendveld <marijn@round84.com>
2021-10-23 17:40:24 +0200

    :mag: Examples for `CalculatePrice_charged_per_minute`

--- PricingEngine/PricingEngine.spec.ts
+++ PricingEngine/PricingEngine.spec.ts
@@ -5,6 +5,8 @@ import {EUR, Money} from "../Money/Money";
 export class PricingEngineSpec {
     @Test()
     @TestCase(EUR(30), DurationInMinutes(1), EUR(30))
+    @TestCase(EUR(30), DurationInMinutes(3), EUR(90))
+    @TestCase(EUR(23), DurationInMinutes(12), EUR(276))
     CalculatePrice_charged_per_minute(pricePerMinute: Money, duration: Duration, totalPrice: Money) {
         const actual = pricingEngine(pricePerMinute, duration);
         const expected = totalPrice;
