f9fe3b6a88846ef2289cc76234c9f841b4df5b60
Marijn Huizendveld <marijn@round84.com>
2021-10-23 13:00:56 +0200

    :skull: Remove useless test.

--- Money/Money.spec.ts
+++ Money/Money.spec.ts
@@ -1,15 +1,6 @@
 import {Expect, Ignore, Test} from 'alsatian';
-import {Euro} from "./Money";
 
 export class MoneySpec {
-    /**
-     * This test is only here to verify that false test results get detected properly in GitHub Actions
-     */
-    @Test()
-    Money_Exposes_Currency_IsoCode_for_Euro() {
-        Expect(Euro).toBe("EUR");
-    }
-
     @Test()
     @Ignore('TODO: Implement this test scenario')
     Money_equalTo_detects_identical_values() {
