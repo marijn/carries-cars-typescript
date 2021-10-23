030be41116c1661be0c8e54420cebbad4573261c
Marijn Huizendveld <marijn@round84.com>
2021-10-23 15:27:31 +0200

    :skull: Remove obsolete test.

--- PricingEngine/PricingEngine.spec.ts
+++ PricingEngine/PricingEngine.spec.ts
@@ -3,14 +3,6 @@ import {DurationInMinutes, pricingEngine} from "./PricingEngine";
 import {EUR} from "../Money/Money";
 
 export class PricingEngineSpec {
-    /**
-     * This test is only here to verify that false test results get detected properly in GitHub Actions
-     */
-    @Test()
-    Test_Fails() {
-        Expect(pricingEngine("", 1)).toBe({});
-    }
-
     @Test()
     CalculatePrice_charged_per_minute() {
         const thirtyCents = EUR(30);
