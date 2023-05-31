34e391726a97e3fc79952a3b020a1940b4838dc0
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 11:33:06 +0200

    :mag: `PricingEngineSpec`
    - `CalculatePrice_charged_per_minute_including_an_extended_reservation`

--- PricingEngine/PricingEngine.spec.ts
+++ PricingEngine/PricingEngine.spec.ts
@@ -15,4 +15,21 @@ export class PricingEngineSpec {
         const expected = totalPrice;
         Expect(actual.equalTo(expected)).toBe(true);
     }
+
+    @Test()
+    CalculatePrice_charged_per_minute_including_an_extended_reservation() {
+        // Arrange
+        const pricePerMinute = EUR(24);
+        const duration = DurationInMinutes(10);
+        const totalReservationDuration = DurationInMinutes(30);
+        // const totalPrice = EUR(0.24 * 10 + (30 - 20) * 0.12);
+        const totalPrice = EUR(288);
+
+        // Act
+        const actual = pricingEngine(pricePerMinute, duration, totalReservationDuration);
+
+        // Assert
+        const expected = totalPrice;
+        Expect(actual.equalTo(expected)).toBe(true);
+    }
 }
